ad15db64818eef24a744ee7523d54ca635818d8d
smcftp <156796937+smcftp@users.noreply.github.com>
2025-02-05 15:15:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import os
 from dash import Dash
 from layouts.main_layout import create_main_layout
 from callbacks import graph_callbacks
 
-# Импорт приложение
+# Импорт приложения
 from config import app
 
 # Инициализация приложения
@@ -12,4 +13,5 @@ app.layout = create_main_layout()
 graph_callbacks.register_graph_callbacks(app)
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    port = int(os.getenv("PORT", 8080))  # Используем порт от Railway
+    app.run_server(host="0.0.0.0", port=port, debug=True)
