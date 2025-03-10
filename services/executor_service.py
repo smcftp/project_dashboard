fd4893a8faa707b19b6cc201c0b5ecdab35bdb6c
smcftp <156796937+smcftp@users.noreply.github.com>
2025-03-10 14:18:09 +0300

    Update executor_service.py

--- services/executor_service.py
+++ services/executor_service.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import func
+from sqlalchemy import func, cast, Numeric
 
 from database import models
 
@@ -17,7 +17,7 @@ def get_executors_data_by_project(db_session: Session, title_id: int):
             modeling_query = (
                 db.query(
                     models.Executor.executor_name.label("name"),
-                    func.round(func.sum(models.ModelingData.total_mass) / 1000000, 2).label("mass")
+                    func.round(func.sum(models.ModelingData.total_mass) / 1000000, 2).label("total_mass_modeling")
                 )
                 .join(models.ModelingData, models.Executor.id == models.ModelingData.executor_id)
                 .join(models.Title, models.Title.id == models.ModelingData.title_id)
@@ -25,23 +25,26 @@ def get_executors_data_by_project(db_session: Session, title_id: int):
                 .group_by(models.Executor.executor_name)
             )
 
-            work_hours_query = (
+            work_hours_query = (    
                 db.query(
                     models.Executor.executor_name.label("name"),
-                    func.sum(models.WorkHoursInWorkSection.hours_worked).label("total_hours"),
-                    func.round(func.sum(models.WorkHoursInWorkSection.hours_worked) / 3, 2).label("plan_mass")
+                    func.round(func.sum(models.WorkHoursInWorkSection.hours_worked), 2).label("total_hours"),
+                    # func.round(func.sum(models.WorkHoursInTekla.hours_worked) / 3, 2).label("plan_mass")
                 )
                 .join(models.WorkHoursInWorkSection, models.Executor.id == models.WorkHoursInWorkSection.executor_id)
                 .join(models.WorksectionTask, models.WorksectionTask.id == models.WorkHoursInWorkSection.task_id)
                 .join(models.Title, models.Title.id == models.WorksectionTask.title_id)
+                .outerjoin(models.WorkHoursInTekla, models.Executor.id == models.WorkHoursInTekla.executor_id)  # ДОБАВЛЕН JOIN
                 .filter(models.Title.id == title_id)
                 .group_by(models.Executor.executor_name)
             )
 
             drawing_query = (
-                db.query(
+                    db.query(
                     models.Executor.executor_name.label("name"),
-                    func.count(models.DrawingData.number_of_drawings).label("completed_drawings")
+                    func.round(func.sum(cast(models.DrawingData.number_of_drawings, Numeric)), 0).label("completed_drawings"),
+                    func.round(func.sum(cast(models.DrawingData.total_complexity, Numeric)), 0).label("total_complexity_drawing"),
+                    func.round(func.sum(cast(models.DrawingData.total_mass, Numeric)), 2).label("total_mass_drawing"),
                 )
                 .join(models.DrawingData, models.Executor.id == models.DrawingData.executor_id)
                 .join(models.Title, models.Title.id == models.DrawingData.title_id)
@@ -52,7 +55,7 @@ def get_executors_data_by_project(db_session: Session, title_id: int):
             tekla_hours_query = (
                 db.query(
                     models.Executor.executor_name.label("name"),
-                    func.sum(models.WorkHoursInTekla.hours_worked).label("tekla_hours")
+                    func.round(func.sum(models.WorkHoursInTekla.hours_worked), 0).label("tekla_hours")
                 )
                 .join(models.WorkHoursInTekla, models.Executor.id == models.WorkHoursInTekla.executor_id)
                 .join(models.Title, models.Title.id == models.WorkHoursInTekla.title_id)
@@ -65,12 +68,17 @@ def get_executors_data_by_project(db_session: Session, title_id: int):
             work_hours_result = work_hours_query.all()
             drawing_result = drawing_query.all()
             tekla_hours_result = tekla_hours_query.all()
+            print("tekla_hours_result = ", tekla_hours_result)
 
             # Преобразование результатов в DataFrame
-            modeling_df = pd.DataFrame(modeling_result, columns=["name", "mass"])
-            work_hours_df = pd.DataFrame(work_hours_result, columns=["name", "total_hours", "plan_mass"])
-            drawing_df = pd.DataFrame(drawing_result, columns=["name", "completed_drawings"])
+            modeling_df = pd.DataFrame(modeling_result, columns=["name", "total_mass_modeling"])
+            work_hours_df = pd.DataFrame(work_hours_result, columns=["name", "total_hours"])
+            drawing_df = pd.DataFrame(drawing_result, columns=["name", "completed_drawings", "total_complexity_drawing", "total_mass_drawing"])
             tekla_hours_df = pd.DataFrame(tekla_hours_result, columns=["name", "tekla_hours"])
+            print("modeling_df = ", modeling_df)
+            print("work_hours_df = ", work_hours_df)
+            print("drawing_df = ", drawing_df)
+            print("tekla_hours_df = ", tekla_hours_df)
 
             # Объединение данных
             result_df = modeling_df.merge(work_hours_df, on="name", how="outer")
